2d1916c11c9fb2ea76a3b1781890005eb18abb60
Basti Buck <mail@bastibuck.de>
2026-01-11 20:41:27 +0100

    add new bricks

--- src/unicorn/Unicorn.tsx
+++ src/unicorn/Unicorn.tsx
@@ -65,7 +65,7 @@ const Unicorn: React.FC = () => {
             duration: 0.6,
             stagger: {
               amount: 0.5,
-              from: "random", // TODO? maybe remove this in favor of having the correct order of building and unbuilding the unicorn?
+              from: "center", // TODO? maybe remove this in favor of having the correct order of building and unbuilding the unicorn?
             },
           },
           0
@@ -81,7 +81,7 @@ const Unicorn: React.FC = () => {
             duration: 0.6,
             stagger: {
               amount: 0.5,
-              from: "random", // TODO? maybe remove this in favor of having the correct order of building and unbuilding the unicorn?
+              from: "center", // TODO? maybe remove this in favor of having the correct order of building and unbuilding the unicorn?
             },
           },
           0
@@ -103,7 +103,7 @@ const Unicorn: React.FC = () => {
       <MaterialDebugConfig />
 
       <group name="unicorn" scale={0.2} ref={unicornRef}>
-        <group name="base" visible={true}>
+        <group name="base" visible={true} position={[0, 0, 0]}>
           <mesh
             geometry={unicorn.meshes["8x4rounded"].geometry}
             position={[brickSize * -2, 0, 0]}
@@ -172,7 +172,7 @@ const Unicorn: React.FC = () => {
             material={BrickMaterial.blueLight}
           />
           <mesh
-            geometry={unicorn.meshes["2x1sloped^2"].geometry}
+            geometry={unicorn.meshes["2x1flatsloped^2"].geometry}
             position={[
               brickSize * 2,
               brickHeight * 1,
@@ -182,7 +182,7 @@ const Unicorn: React.FC = () => {
             material={BrickMaterial.blueLight}
           />
           <mesh
-            geometry={unicorn.meshes["2x1sloped^2"].geometry}
+            geometry={unicorn.meshes["2x1flatsloped^2"].geometry}
             position={[
               brickSize * -2,
               brickHeight * 1,
@@ -198,7 +198,7 @@ const Unicorn: React.FC = () => {
             material={BrickMaterial.orange}
           />
           <mesh
-            geometry={unicorn.meshes["2x1sloped^2"].geometry}
+            geometry={unicorn.meshes["2x1flatsloped^2"].geometry}
             position={[
               brickSize * 2,
               brickHeight * 1,
@@ -208,53 +208,53 @@ const Unicorn: React.FC = () => {
             material={BrickMaterial.orange}
           />
           <mesh
-            geometry={unicorn.meshes["2x1sloped^2"].geometry}
+            geometry={unicorn.meshes["2x1flatsloped^2"].geometry}
             position={[brickSize * -2, brickHeight, brickSize * -1.5]}
             material={BrickMaterial.orange}
           />
 
           <mesh
-            geometry={unicorn.meshes["2x1sloped^2"].geometry}
+            geometry={unicorn.meshes["2x1flatsloped^2"].geometry}
             position={[brickSize * -1, brickHeight * 1, brickSize * -2.5]}
             material={BrickMaterial.red}
           />
           <mesh
-            geometry={unicorn.meshes["2x1sloped^2"].geometry}
+            geometry={unicorn.meshes["2x1flatsloped^2"].geometry}
             position={[brickSize * 1, brickHeight * 1, brickSize * -2.5]}
             rotation={[0, fullRotation * 0.5, 0]}
             material={BrickMaterial.red}
           />
           <mesh
-            geometry={unicorn.meshes["2x1sloped^2"].geometry}
+            geometry={unicorn.meshes["2x1flatsloped^2"].geometry}
             position={[brickSize * -1, brickHeight * 1, brickSize * 2.5]}
             material={BrickMaterial.purple}
           />
           <mesh
-            geometry={unicorn.meshes["2x1sloped^2"].geometry}
+            geometry={unicorn.meshes["2x1flatsloped^2"].geometry}
             position={[brickSize * 1, brickHeight * 1, brickSize * 2.5]}
             rotation={[0, fullRotation * 0.5, 0]}
             material={BrickMaterial.purple}
           />
 
           <mesh
-            geometry={unicorn.meshes["3x1sloped^3"].geometry}
+            geometry={unicorn.meshes["3x1flatsloped^3"].geometry}
             position={[brickSize * 2.5, brickHeight * 1, brickSize * -0.5]}
             rotation={[0, fullRotation * 0.5, 0]}
             material={BrickMaterial.yellow}
           />
           <mesh
-            geometry={unicorn.meshes["3x1sloped^3"].geometry}
+            geometry={unicorn.meshes["3x1flatsloped^3"].geometry}
             position={[brickSize * -2.5, brickHeight * 1, brickSize * -0.5]}
             material={BrickMaterial.yellow}
           />
 
           <mesh
-            geometry={unicorn.meshes["3x1sloped^3"].geometry}
+            geometry={unicorn.meshes["3x1flatsloped^3"].geometry}
             position={[brickSize * -2.5, brickHeight * 1, brickSize * 0.5]}
             material={BrickMaterial.turquoiseDark}
           />
           <mesh
-            geometry={unicorn.meshes["3x1sloped^3"].geometry}
+            geometry={unicorn.meshes["3x1flatsloped^3"].geometry}
             position={[brickSize * 2.5, brickHeight * 1, brickSize * 0.5]}
             rotation={[0, fullRotation * 0.5, 0]}
             material={BrickMaterial.turquoiseDark}
@@ -345,6 +345,18 @@ const Unicorn: React.FC = () => {
             material={BrickMaterial.blueLight}
           />
 
+          <mesh
+            geometry={unicorn.meshes["2x1slantedconnector"].geometry}
+            position={[0, brickHeight * 6, brickSize * -2]}
+            rotation={[fullRotation * 0, fullRotation * 0.5, fullRotation * 0]}
+            material={BrickMaterial.turquoiseDark}
+          />
+          <mesh
+            geometry={unicorn.meshes["1x1flatsloped^2"].geometry}
+            position={[0, brickHeight * 9, brickSize * -2.5]}
+            rotation={[fullRotation * 0, fullRotation * 0.25, fullRotation * 0]}
+            material={BrickMaterial.turquoiseDark}
+          />
           {/* <mesh
             geometry={unicorn.meshes["1x1"].geometry}
             position={[locator.x, locator.y, locator.z]}
