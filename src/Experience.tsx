d68e873852455c69a48d5be91d888854ce9a0529
Basti Buck <mail@bastibuck.de>
2026-01-14 19:55:52 +0100

    allow passing the speed from the URL

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,22 +1,37 @@
-import { Stage } from "@react-three/drei";
+import { OrbitControls, Stage } from "@react-three/drei";
 import Unicorn from "./unicorn/Unicorn";
+import { parseAsInteger, useQueryState } from "nuqs";
 
 const Experience: React.FC = () => {
+  const [speed] = useQueryState("speed", parseAsInteger.withDefault(1));
+
   return (
-    <Stage
-      adjustCamera={false}
-      shadows={{
-        type: "contact",
-        opacity: 0.35,
-        blur: 1.6,
-      }}
-      environment={{
-        environmentIntensity: 0.5,
-        preset: "studio",
-      }}
-    >
-      <Unicorn />
-    </Stage>
+    <>
+      <Stage
+        adjustCamera={false}
+        shadows={{
+          type: "contact",
+          opacity: 0.35,
+          blur: 1.6,
+        }}
+        environment={{
+          environmentIntensity: 0.5,
+          preset: "studio",
+        }}
+      >
+        <Unicorn />
+      </Stage>
+
+      <OrbitControls
+        autoRotate
+        autoRotateSpeed={speed}
+        enablePan={false}
+        minPolarAngle={0.35}
+        maxPolarAngle={1.5}
+        minDistance={12}
+        maxDistance={45}
+      />
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,28 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Experience from "./Experience.tsx";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import PerformanceMonitor from "./debug/PerformanceMonitor.tsx";
 import DebugControls from "./debug/DebugControls.tsx";
+import { NuqsAdapter } from "nuqs/adapters/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Canvas dpr={[1, 2]} camera={{ position: [14, 12, 15] }}>
-      <Experience />
+    <NuqsAdapter>
+      <Canvas dpr={[1, 2]} camera={{ position: [14, 12, 15] }}>
+        <Experience />
+        <PerformanceMonitor />
+      </Canvas>
 
-      <OrbitControls
-        autoRotate
-        autoRotateSpeed={0.4}
-        enablePan={false}
-        minPolarAngle={0.35}
-        maxPolarAngle={1.5}
-        minDistance={12}
-        maxDistance={45}
-      />
-
-      <PerformanceMonitor />
-    </Canvas>
-
-    <DebugControls />
+      <DebugControls />
+    </NuqsAdapter>
   </StrictMode>
 );
