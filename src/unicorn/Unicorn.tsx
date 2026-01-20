c231cb6d86ffd037c07798f32ea9fc521e69aa02
Basti Buck <mail@bastibuck.de>
2026-01-20 11:19:25 +0100

    fix pos

--- src/unicorn/Unicorn.tsx
+++ src/unicorn/Unicorn.tsx
@@ -543,7 +543,7 @@ const Unicorn: React.FC = () => {
         <group
           name="front-legs"
           visible={visibility.frontLegs}
-          position={[brickSize * 6, brickHeight * 3, 0]}
+          position={[brickSize * 8, brickHeight * 3.5, 0]}
         >
           <group
             name="legs"
